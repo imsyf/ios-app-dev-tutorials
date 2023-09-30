68b75a0b8e00b58c0bf80306e58b77a456df8022
imsyf <alghozalyi@gmail.com>
2023-09-30 20:30:09 +0700

    Create Xcode project for `Scrumdinger`

---
+++ 01-SwiftUI/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,16 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Imam on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
