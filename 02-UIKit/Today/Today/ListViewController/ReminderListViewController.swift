478fd5cf2cb1a070ede885b0402f995664d1b9c1
imsyf <alghozalyi@gmail.com>
2023-10-07 16:28:28 +0700

    Make reminders identifiable

--- 02-UIKit/Today/Today/ListViewController/ReminderListViewController.swift
+++ 02-UIKit/Today/Today/ListViewController/ReminderListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     var dataSource: DataSource!
+    var reminders: [Reminder] = Reminder.sampleData
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,7 @@ class ReminderListViewController: UICollectionViewController {
         
         dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView,
                                                                    indexPath: IndexPath,
-                                                                   itemIdentifier: String) in
+                                                                   itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
                                                                 for: indexPath,
                                                                 item: itemIdentifier)
@@ -28,7 +29,7 @@ class ReminderListViewController: UICollectionViewController {
         
         var snapshot = Snapshot()
         snapshot.appendSections([0])
-        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        snapshot.appendItems(reminders.map { $0.id })
         dataSource.apply(snapshot)
         
         collectionView.dataSource = dataSource
