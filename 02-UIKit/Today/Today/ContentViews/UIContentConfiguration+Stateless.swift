5c479563d8b1d5a923120a68da4861ab5649f20d
imsyf <alghozalyi@gmail.com>
2023-10-08 09:27:13 +0700

    Edit reminder title

---
+++ 02-UIKit/Today/Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Imam on 08/10/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
